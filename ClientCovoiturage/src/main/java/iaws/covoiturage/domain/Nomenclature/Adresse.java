89d444ae7585ef840f773d7c90effa50fc430cf2
gentuxx <loic.martinux@gmail.com>
2013-03-30 17:51:01 +0100

    Adding basics classes

---
+++ ClientCovoiturage/src/main/java/iaws/covoiturage/domain/Nomenclature/Adresse.java
@@ -0,0 +1,18 @@
+package iaws.covoiturage.domain.Nomenclature;
+
+public class Adresse {
+	
+	private String adresse;
+	
+	public Adresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+}
---
+++ ClientCovoiturage/src/main/java/iaws/covoiturage/domain/Nomenclature/Prenom.java
@@ -0,0 +1,18 @@
+package iaws.covoiturage.domain.Nomenclature;
+
+public class Prenom {
+	
+	String prenom;
+	
+	public Prenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+}

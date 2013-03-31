84da8ce45647a581fc48eea21f9ef50a351d0f46
gentuxx <loic.martinux@gmail.com>
2013-03-31 23:22:00 +0200

    Inscription implementation

---
+++ ClientCovoiturage/src/main/java/services/impl/InscriptionImpl.java
@@ -0,0 +1,16 @@
+package services.impl;
+
+import iaws.covoiturage.domain.nomenclature.Adresse;
+import iaws.covoiturage.domain.nomenclature.Mail;
+import iaws.covoiturage.domain.nomenclature.Nom;
+import iaws.covoiturage.domain.nomenclature.Prenom;
+import services.InscriptionService;
+
+public class InscriptionImpl implements InscriptionService {
+
+	public String inscrire(Nom myName, Prenom myFirstName, Mail myMail,
+			Adresse myAddress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
